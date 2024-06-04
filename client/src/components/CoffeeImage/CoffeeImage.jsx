dd5818a08e8f364776838a98f83c8d059eaf7276
kimntan <kim.thd.nguyen@gmail.com>
2024-06-04 11:36:22 -0400

    Add placeholder coffee bag for beans with no image.

--- client/src/components/CoffeeImage/CoffeeImage.jsx
+++ client/src/components/CoffeeImage/CoffeeImage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import PlainCoffeeBag from '../../assets/images/placeholder-coffee.jpeg';
 import './CoffeeImage.scss';
 
 export default function CoffeeImage({bean, back}) {
@@ -6,7 +7,7 @@ export default function CoffeeImage({bean, back}) {
 
   return (
     <div className="coffee-image">
-      <img src={bean ? bean.image : null} alt={bean ? bean.bean_name : null} className="coffee-image__image"></img>
+      <img src={bean ? bean.image ? bean.image : PlainCoffeeBag : null} alt={bean ? bean.bean_name : null} className="coffee-image__image"></img>
       <button className="coffee-image__back" onClick={() => navigate(back)}>Back</button>
     </div>
   )
