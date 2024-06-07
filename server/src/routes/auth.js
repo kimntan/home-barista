a6ddceeadba17e60d68d8a0479cc0b9260fda82a
kimntan <kim.thd.nguyen@gmail.com>
2024-06-07 09:06:03 -0400

    Update variables.

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -7,7 +7,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
 router.use(session({
-  secret: process.env.SECRET,
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 1000 * 60 * 60 * 24 }
