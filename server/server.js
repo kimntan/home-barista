6214df5178d66b270642e1b3bb9c89d2028bfe1b
kimntan <kim.thd.nguyen@gmail.com>
2024-06-09 11:18:39 -0400

    Debugging.

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+app.set("trust proxy", 1);
 
 const isAuth = (req, res, next) => {
   if (!req.user) {
