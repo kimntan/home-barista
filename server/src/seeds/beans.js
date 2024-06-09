8ca0dd6b8be04afeae4f0280b943676fad872073
kimntan <kim.thd.nguyen@gmail.com>
2024-06-08 21:38:19 -0400

    Remove seed files for production.

--- server/src/seeds/beans.js
+++
@@ -1,6 +0,0 @@
-const data = require('../seed-data/beans');
-
-exports.seed = async function(knex) {
-  await knex('beans').del()
-  await knex('beans').insert(data);
-};
--- server/src/seeds/recipes.js
+++
@@ -1,6 +0,0 @@
-const data = require('../seed-data/recipes.js');
-
-exports.seed = async function(knex) {
-  await knex('recipes').del()
-  await knex('recipes').insert(data);
-};
